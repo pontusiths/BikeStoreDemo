f0b472688bb41379531aa0a2d639bd8f77c64b7e
Pontus <pontus.lindgren@iths.se>
2020-01-09 12:57:59 +0100

    Added BikeStore Demo

---
+++ BikeStoreDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using BikeStoreDemo.Models;
+using System;
+using System.Linq;
+
+namespace BikeStoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bikeStoreContext = new BikeStores2Context();
+            
+            foreach (var item in bikeStoreContext.Customers)
+            {
+                Console.WriteLine($"{item.FirstName}, {item.LastName}");
+            }
+        }
+    }
+}
